a8954c9f30d6a687a0e5a74615f9c528df798920
Viktor Tumanian <victumanyan@gmail.com>
2023-12-27 07:23:36 +0400

    made template

---
+++ lexicon/lexicon_ru.py
@@ -0,0 +1,16 @@
+LEXICON_COMMANDS_RU: dict[str, str] = {
+    '/command_1': 'command_1 desription',
+    '/command_2': 'command_2 desription',
+    '/command_3': 'command_3 desription',
+    '/command_4': 'command_4 desription'
+}
+
+LEXICON_INLINE_BUTTONS: dict[str, str] = {
+    'but_1': 'Кнопка 1',
+    'but_2': 'Кнопка 2',
+    'but_3': 'Кнопка 3',
+    'but_4': 'Кнопка 4',
+    'but_5': 'Кнопка 5',
+    'but_6': 'Кнопка 6',
+    'but_7': 'Кнопка 7',
+}
